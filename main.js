88bdc6906f6d834544bea004a03bca521f7c5bb3
Dan Cebotarenco <dcebotarenco@gmail.com>
2016-10-11 10:29:01 +0300

    do a hht get

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var Logger = require('./logger/logger');
 var GoogleConnection = require('./google/googleConnection.js');
 var ModelBuilder = require('./modelBuilder/ModelBuilder.js');
 var Cron = require('node-cron');
+var http = require('http');
 
 Logger.logger().info("Starting App");
 GoogleConnection.fetchBotSettings((response)=>function (rows) {
@@ -13,6 +14,7 @@ GoogleConnection.fetchBotSettings((response)=>function (rows) {
     let skypeBot = new SkypeBot(settings);
     restServer.attachBot(skypeBot, "/api/messages");
     Cron.schedule(settings.getValueByKey('cron_updateSettings'), function (skypeBot) {
+        http.get(settings.getValueByKey('bot_url'));
         GoogleConnection.fetchBotSettings((response) => function (rows) {
             let settings = ModelBuilder.createBotSettings(rows);
             skypeBot.updateSettings(settings);
