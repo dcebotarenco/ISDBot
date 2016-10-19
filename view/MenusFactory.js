64fe1b27b08ed3a419b373595612d36e7580a57d
Dan Cebotarenco <dcebotarenco@gmail.com>
2016-10-19 18:47:48 +0300

    Skip menu if it has no meal

--- view/MenusFactory.js
+++ view/MenusFactory.js
@@ -7,16 +7,20 @@ var MenusView = require('../view/MenusView');
 var Logger = require('../logger/logger');
 
 class MenusFactory {
-    static buildMenus(session,day) {
+    static buildMenus(session, day) {
         Logger.logger().info("Building Day Menus View");
         let menuList = [];
-        day.menuList.forEach(function(menu,index){
+        day.menuList.forEach(function (menu, index) {
             let buttonList = [];
-            menu.mealGroups.forEach(function(group,index)
-            {
+            menu.mealGroups.forEach(function (group, index) {
                 buttonList.push(new Button(session, menu.constructor.name, group.groupName));
             });
-            menuList.push(new Menu(session, menu.name, menu.constructor.name, menu.mealsList, buttonList));
+            let menuMealsWithName = menu.mealsList.filter(function (meal) {
+                return meal.name.length > 0;
+            });
+            if (menuMealsWithName.length > 0) {
+                menuList.push(new Menu(session, menu.name, menu.constructor.name, menu.mealsList, buttonList));
+            }
         });
         let dayMenu = new MenusView(session, menuList);
         Logger.logger().info("View of Menus created");
