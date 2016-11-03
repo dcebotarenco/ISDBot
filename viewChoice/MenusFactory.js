66b2bd016dd464795904c48185f011bddeaf9d48
Constantin Harea <rhoklin@gmail.com>
2016-11-03 10:27:06 +0200

    Removed subtitle from Hero Cards since it reduces space for text in the card

--- viewChoice/MenusFactory.js
+++ viewChoice/MenusFactory.js
@@ -17,9 +17,10 @@ class MenusFactory {
             let mealGroup = menuObj.menu.mealGroups.filter(function (mealGroup) {
                return mealGroup.groupName ==  menuObj.menuName;
             });
+            let title = "" + menuObj.menu.name + " (" + menuObj.menuNumber + menuObj.menuName + ")";
             /*not so nice with index, to be changed later : mealGroup[0].meals*/
             buttonList.push(new Button(session, menuObj.menu.constructor.name, menuObj.menuName));
-            menuList.push(new Menu(session, menuObj.menu.name, menuObj.menuNumber.concat(menuObj.menuName), menuObj.menu.constructor.name, mealGroup[0].meals, buttonList));
+            menuList.push(new Menu(session, title, /*menuObj.menu.name, menuObj.menuNumber.concat(menuObj.menuName),*/ menuObj.menu.constructor.name, mealGroup[0].meals, buttonList));
         });
         let dayMenu = new MenusView(session, menuList);
         Logger.logger().info("View of Menus created");
