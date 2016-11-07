13f9ba92ef5156185717d992fc1e1054b6c0f52a
Dan Cebotarenco <dcebotarenco@gmail.com>
2016-11-07 19:15:53 +0200

    add more explicit catch of error

--- skypebot/skypebot.js
+++ skypebot/skypebot.js
@@ -29,7 +29,7 @@ class SkypeBot {
         });
         this.bot = new botbuilder.UniversalBot(this.botConnection);
         this.bot.on('error', function (error) {
-            Logger.logger().error("ERROR[%s]", error.message);
+            Logger.logger().error("Proxy Caught ERROR[%s] [%s] ", error.message, error.stack);
         });
         this.bot.on('contactRelationUpdate', SkypeBot.onBotAddedInContacts);
         this.bot.use({botbuilder: SkypeBot.proxy});
